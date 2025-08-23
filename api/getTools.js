ef8d4acedc4b2785be78a7d0a0362f2531f029f1
littleinca <littleinca@gmail.com>
2025-08-23 23:02:08 +0200

    Create getTools.js

---
+++ api/getTools.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
+export default async function handler(req,res){
+  const {data,error}=await supabase.from('tools').select('*');
+  if(error) return res.status(500).json({error:error.message});
+  res.status(200).json(data);
+}
